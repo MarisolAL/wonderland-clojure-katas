8f161b5b0c875c66310b720e60be34e01754413f
Marisol <marisolamezcua@ciencias.unam.mx>
2019-08-05 13:53:44 -0500

    Avance en verificacion
    Problemas al cargar el diccionario.

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,17 @@
                (slurp)
                (read-string)))
 
+;;Funcion que verifica si el cambio en la posicion n es valido,
+;;para esto se revisa que este en el diccionario la modificada
+(defn change? [word1 word2 n]
+  (let [new-letter (nth word2 n)
+        last-w (conj (take-last (- (count word1) (inc n)) word1) new-letter)
+        new-word (cons (take n word1) last-w)
+      ]
+    ;;Checar si esta en el diccionario
+      ( )))
+
+(change? "holamundo" "abcabcabc" 4)
+
 (defn doublets [word1 word2]
   "make me work")
