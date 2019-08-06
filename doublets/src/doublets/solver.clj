9271acbfba336b42f4ea4069f3fc238c194df803
Marisol <marisolamezcua@ciencias.unam.mx>
2019-08-06 09:24:17 -0500

    Revisa en diccionario.

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -2,8 +2,9 @@
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]))
 
-(def words (-> "words.edn"
-               (io/resource)
+(+ 1 1)
+
+(def words (-> "/home/marisol/Documentos/ClojureEjemplos/wonderland-clojure-katas/doublets/resources/words.edn"
                (slurp)
                (read-string)))
 
@@ -12,12 +13,15 @@
 (defn change? [word1 word2 n]
   (let [new-letter (nth word2 n)
         last-w (conj (take-last (- (count word1) (inc n)) word1) new-letter)
-        new-word (cons (take n word1) last-w)
+        new-word (apply str (concat (take n word1) last-w))
       ]
     ;;Checar si esta en el diccionario
-      ( )))
+      (if (= -1 (.indexOf words new-word)) false true)
+  ))
 
 (change? "holamundo" "abcabcabc" 4)
+(change? "door" "boor" 0)
+
 
 (defn doublets [word1 word2]
   "make me work")
